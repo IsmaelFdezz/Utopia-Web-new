97fa6f7a86a2827d29af15fd5183a01e90e66e19
Ismael Fernandez <ismaelfdc2004@gmail.com>
2024-05-18 14:54:25 -0300

    header con el punto

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -31,7 +31,7 @@ function Header(props: HeaderProps) {
     <header className="bg-white fixed top-0 left-0 right-0 z-50 shadow-lg">
       <Marquee className="bg-[#004080] text-white p-2">
         <p className="text-xs mx-2">
-          ENVIOS GRATIS A PARTIR DE $50000
+          ENVÍO GRATIS A PARTIR DE $50.000
         </p>
       </Marquee>
       <nav className="mx-auto grid grid-cols-3 p-2">
